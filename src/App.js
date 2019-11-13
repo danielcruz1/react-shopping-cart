47222c61643a93cd9d9cb872e0f1c9b06daf7119
Daniel Cruz <d.cruz.489@hotmail.com>
2019-11-13 10:17:01 -0700

    part 4

--- src/App.js
+++ src/App.js
@@ -18,17 +18,16 @@ function App() {
 	};
 
 	return (
-		<div className="App">
-			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
+		<ProductContext.Provider value={{ products, addItem }}>
 
-				{/* Routes */}
-				<Route exact path="/" component={Products}/>
+			<div className="App">
+					<Navigation cart={cart} />
+					{/* Routes */}
+					<Route exact path="/" component={Products}/>
+					<Route path="/" component={addItem} />}
+			</div>
 			
-				<Route path="/" component={addItem} />}
-				
-			</ProductContext.Provider>	
-		</div>
+		</ProductContext.Provider>	
 	);
 }
 
